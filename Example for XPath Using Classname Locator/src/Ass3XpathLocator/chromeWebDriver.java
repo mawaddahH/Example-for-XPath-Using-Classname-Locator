21e23f8cf945850d12c8a1bbb246d98891d9d946
lo0ol <lo0ol@MWDH-Work2>
2022-07-24 21:49:34 +0300

    First Commit

---
+++ Example for XPath Using Classname Locator/src/Ass3XpathLocator/chromeWebDriver.java
@@ -0,0 +1,35 @@
+package Ass3XpathLocator;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class chromeWebDriver {
+	public WebDriver driver;
+
+  @Test
+  public void f() throws InterruptedException {
+	  System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\lo0ol\\" + "Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		// Navigate to a website
+		driver.navigate().to("https://www.elm.sa/en/Pages/default.aspx");
+		
+		// Mazimize current window
+		driver.manage().window().maximize();
+		
+		// define the web element to find it
+		WebElement classMethod = driver.findElement(By.xpath("//a[@class='btn btn-primary btn-lg']"));
+		
+		classMethod.click();
+	
+		// Delay execution for 5 seconds after find specific web element
+		Thread.sleep(5000);
+		
+		// Close the browser
+		driver.quit();
+  }
+}
